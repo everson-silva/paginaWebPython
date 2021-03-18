2237d620f1bfbcf84b7c9436846e8563989a0a83
everson-silva <eversonmessias@outlook.com>
2021-03-18 21:55:55 +0000

    Fazendo o login

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template, request
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Feulo'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html" , title="Login")
+
--- microblog.py
+++ microblog.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask("microblog")
-
-#aqui vai comentario
-@app.route("/")
-def index():
-    return "Olá Mundo"
-
-app.run()
+from app import app
